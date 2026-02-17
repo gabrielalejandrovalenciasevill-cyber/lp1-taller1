8cf09d123315496b7071a343a3c3f8e32f4f789b
Gabriel Alejandro Valencia Sevillano <gabrielalejandrovalenciasevill@gmail.com>
2026-02-16 20:28:19 -0500

    feat: ajustes problema 1

--- problema1/main.go
+++ problema1/main.go
@@ -2,34 +2,34 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 )
 
 // Objetivo: Lanzar varias goroutines que imprimen mensajes y esperar a que todas terminen.
-// TODO: Completa los pasos marcados con TODO para entender goroutines y WaitGroup.
+// : Completa los pasos marcados con TODO para entender goroutines y WaitGroup.
 
 func worker(id int, veces int, wg *sync.WaitGroup) {
-	// TODO: asegurar que al finalizar la función se haga wg.Done()
-	
+	// : asegurar que al finalizar la función se haga wg.Done()
+
 	for i := 1; i <= veces; i++ {
 		fmt.Printf("[worker %d] hola %d\n", id, i)
-		// TODO: dormir un poco para simular trabajo (p. ej. 100–300 ms)
-		time.sleep(time.Dururacion( 100000) * time.millisecond))
-
+		// : dormir un poco para simular trabajo (p. ej. 100–300 ms)
+		time.Sleep(time.Duration(100+rand.Intn(201)) * time.Millisecond)
 	}
-    wg.done()
+	wg.Done()
 
- }
+}
 
 func main() {
 	var wg sync.WaitGroup
 
-	// TODO: cambiar estos parámetros y observar el intercalado de salidas
-	// numGoroutines := 3
-	// veces := 4
+	//  cambiar estos parámetros y observar el intercalado de salidas
+	// numGoroutines := 4
+	// veces := 1
 
-	// TODO: lanzar varias goroutines, sumar al WG y esperar con wg.Wait()
+	//  lanzar varias goroutines, sumar al WG y esperar con wg.Wait()
 	for id := 1; id <= numGoroutines; id++ {
 		wg.add(1)
 		go worker(id, veces, &wg)
@@ -37,6 +37,6 @@ func main() {
 	}
 
 	// Esperar a que todas las goroutines terminen
-	
+
 	fmt.Println("Listo: todas las goroutines terminaron.")
 }
